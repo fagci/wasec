c902b3e16876def649672f96ba9284b179344cfe
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-03 19:45:30 +0700

    better output

--- wasec.py
+++ wasec.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 from html import unescape
 from random import choices
 from re import MULTILINE, findall
@@ -11,42 +12,61 @@ from requests import Session
 M_RE = r'[\w\.-]+@[\w\.-]+\.\w+'
 P_RE = r'\+?\d{1,4}?[-\s]?\(?\d{1,3}?\)?[-\s]?\d{1,4}[-\s]?\d{1,4}[-\s]?\d{1,9}'
 D_RE = r'^Disallow: (.*)$'
+
 FAKE_PATH = '/' + ''.join(choices(ascii_lowercase, k=12))
+PATHS = (FAKE_PATH, '/.htaccess', '/.git/HEAD', '/../../../../../etc/passwd')
+
 session = Session()
+session.headers['User-Agent'] = 'Mozilla/5.0'
 
 
 def check(uri, res={}):
-    print(f'{Fore.BLUE}[*]{Fore.WHITE}', uri)
+    print(f'{Fore.BLUE}[*]', uri)
+
     r = session.get(uri)
-    print(f'  {Fore.YELLOW}{r.status_code}{Fore.RESET} {len(r.content)} bytes')
+    s_c = Fore.GREEN
+    if 300 <= r.status_code < 400:
+        s_c = Fore.YELLOW
+    elif 400 <= r.status_code < 500:
+        s_c = Fore.WHITE
+    elif r.status_code >= 500:
+        s_c = Fore.RED
+    print(f'{s_c}{r.status_code}{Fore.RESET} {len(r.content)} bytes')
 
     res_results = {}
     for k, re in res.items():
         items = set(findall(re, unescape(r.text), MULTILINE))
         if items:
             res_results[k] = items
-            print(f'  {Fore.GREEN}[+] {k}:{Fore.RESET}', ', '.join(items))
 
     return r, res_results
 
 
 def main(target):
     colorama_init()
-    session.headers['User-Agent'] = 'Mozilla/5.0'
+    contact_res = {'Mails': M_RE, 'Phones': P_RE}
+    loot = []
 
-    paths = (FAKE_PATH, '/.htaccess', '/.git/HEAD',
-             '/../../../../../etc/passwd')
-
-    check(target, {'Mails': M_RE, 'Phones': P_RE})
+    check(target, contact_res)
     _, res = check('%s/robots.txt' % target, {'Disallows': D_RE})
+    loot.append(res)
 
     for path in res.get('Disallows', []):
         uri = f'{target}{path}'
-        check(uri, {'Mails': M_RE, 'Phones': P_RE})
+        _, res = check(uri, contact_res)
+        loot.append(res)
 
-    for path in paths:
+    for path in PATHS:
         uri = f'{target}{path}'
-        check(uri)
+        _, res = check(uri)
+        loot.append(res)
+
+    loot_all = defaultdict(set)
+    for part in loot:
+        for k, v in part.items():
+            loot_all[k] |= v
+
+    print(*[f'{Fore.GREEN}[+]{Fore.WHITE} {k}:{Fore.RESET} {", ".join(v)}' for k,v in loot_all.items()], sep='\n')
 
 
 if __name__ == '__main__':
