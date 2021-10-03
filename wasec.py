cbd92fc37ea0c3b5fb48ab43149a350015fd91d3
Mikhail Yudin <fagci.nsk@gmail.com>
2021-10-03 20:49:20 +0700

    better output

--- wasec.py
+++ wasec.py
@@ -16,22 +16,31 @@ D_RE = r'^Disallow: (.*)$'
 FAKE_PATH = '/' + ''.join(choices(ascii_lowercase, k=12))
 PATHS = (FAKE_PATH, '/.htaccess', '/.git/HEAD', '/../../../../../etc/passwd')
 
+STATUS_COLORS = [
+    Fore.WHITE, Fore.GREEN, Fore.GREEN, Fore.BLUE, Fore.WHITE, Fore.RED
+]
+
+BANNER = r"""
+__      ____ _ ___  ___  ___ 
+\ \ /\ / / _` / __|/ _ \/ __|
+ \ V  V / (_| \__ \  __/ (__ 
+  \_/\_/ \__,_|___/\___|\___| by fagci
+                             
+""".strip()
+
 session = Session()
 session.headers['User-Agent'] = 'Mozilla/5.0'
 
 
-def check(uri, res={}):
-    print(f'{Fore.BLUE}[*]', uri)
+def check(target, path='/', res={}):
+    uri = f'{target}{path}'
+    print(f'{Fore.BLUE}[*]', path, end=' ')
 
     r = session.get(uri)
-    s_c = Fore.GREEN
-    if 300 <= r.status_code < 400:
-        s_c = Fore.YELLOW
-    elif 400 <= r.status_code < 500:
-        s_c = Fore.WHITE
-    elif r.status_code >= 500:
-        s_c = Fore.RED
-    print(f'{s_c}{r.status_code}{Fore.RESET} {len(r.content)} bytes')
+    s_c = STATUS_COLORS[r.status_code // 100]
+
+    print(f'\r{s_c}[i]', r.status_code, f'{path:<17}',
+          f'{len(r.content):>8,}'.replace(',', ' '), 'bytes', Fore.RESET)
 
     res_results = {}
     for k, re in res.items():
@@ -44,29 +53,37 @@ def check(uri, res={}):
 
 def main(target):
     colorama_init()
+    print('=' * 40)
+    print(BANNER)
+    print('=' * 40)
     contact_res = {'Mails': M_RE, 'Phones': P_RE}
     loot = []
 
-    check(target, contact_res)
-    _, res = check('%s/robots.txt' % target, {'Disallows': D_RE})
+    check(target, '/', contact_res)
+    _, res = check(target, '/robots.txt', {'Disallows': D_RE})
     loot.append(res)
 
+    print('Disallows:', '-' * 29)
+
     for path in res.get('Disallows', []):
-        uri = f'{target}{path}'
-        _, res = check(uri, contact_res)
+        _, res = check(target, path, contact_res)
         loot.append(res)
 
+    print('Vulns:', '-' * 33)
+
     for path in PATHS:
-        uri = f'{target}{path}'
-        _, res = check(uri)
+        _, res = check(target, path)
         loot.append(res)
 
     loot_all = defaultdict(set)
+
     for part in loot:
         for k, v in part.items():
             loot_all[k] |= v
 
-    print(*[f'{Fore.GREEN}[+]{Fore.WHITE} {k}:{Fore.RESET} {", ".join(v)}' for k,v in loot_all.items()], sep='\n')
+    print('Loot:', '=' * 34)
+    for k, v in loot_all.items():
+        print(f'{Fore.GREEN}[+]{Fore.WHITE} {k}:{Fore.RESET} {", ".join(v)}')
 
 
 if __name__ == '__main__':
